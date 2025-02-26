b4bc155891c08092e62bb596bc8056da48f49b87
aabid2947 <aaabidhussainpas@gmail.com>
2025-02-26 09:27:33 +0530

    m

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: "*", // Add allowed frontends here
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // The allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be included in requests
 };
 
